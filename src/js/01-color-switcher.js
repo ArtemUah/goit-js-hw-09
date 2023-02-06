b9dab26d7899899b3685748c3b92ba9d6aa76285
Artem Shukatka <artem.shukatka@gmail.com>
2023-02-06 12:58:40 +0200

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const colorChangeInterval = setInterval(getRandomHexColor, 1000);
+let timerId = null;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+});
+
+function changeOfBodyColor(event) {
+  timerId = setInterval(getRandomHexColor, 1000);
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
